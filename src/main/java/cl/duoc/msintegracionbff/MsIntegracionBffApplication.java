ea77e4e1ec410415f289c30e657e8f59138d2163
juazocar <ju.azocar@profesor.duoc.cl>
2023-05-02 19:15:16 -0400

    commit

---
+++ src/main/java/cl/duoc/msintegracionbff/MsIntegracionBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.msintegracionbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIntegracionBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIntegracionBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/msintegracionbff/MsIntegracionBffApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.msintegracionbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIntegracionBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
